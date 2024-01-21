c342c75232136c83488ae2ce65b5f18862172299
newdlops <newdlops@gmail.com>
2024-01-21 13:56:45 +0900

    parsely locale(ko)

--- mazerango/static/mazer/static/js/pages/parsley.js
+++ mazerango/static/mazer/static/js/pages/parsley.js
@@ -1,3 +1,5 @@
+$(`form[attribute=data-parsley-validate]`).parsley();
+
 $.extend(window.Parsley.options, {
   focus: "first",
   excluded:
@@ -17,39 +19,3 @@ $.extend(window.Parsley.options, {
   errorTemplate: "<span></span>",
 })
 
-Parsley.on("field:validated", function (el) {
-  var elNode = $(el)[0]
-  if (elNode && !elNode.isValid()) {
-    var rqeuiredValResult = elNode.validationResult.filter(function (vr) {
-      return vr.assert.name === "required"
-    })
-    if (rqeuiredValResult.length > 0) {
-      var fieldNode = $(elNode.element)
-      var formGroupNode = fieldNode.closest(".form-group")
-      var lblNode = formGroupNode.find(".form-label:first")
-      if (lblNode.length > 0) {
-        // change default error message to include field label
-        var errorNode = formGroupNode.find(
-          "div.parsley-error span[class*=parsley-]"
-        )
-        if (errorNode.length > 0) {
-          var lblText = lblNode.text()
-          if (lblText) {
-            errorNode.html(lblText + " is required.")
-          }
-        }
-      }
-    }
-  }
-})
-
-Parsley.addValidator("restrictedCity", {
-  requirementType: "string",
-  validateString: function (value, requirement) {
-    value = (value || "").trim()
-    return value === "" || value.toLowerCase() === requirement.toLowerCase()
-  },
-  messages: {
-    en: 'You have to live in <a href="https://www.google.com/maps/place/Jakarta">Jakarta</a>.',
-  },
-})
